171a6ececf1204bd37c1de51c18029566fe110f3
Aaronfk07 <ghost.aaron@gmx.at>
2024-09-13 12:31:17 +0200

    IF_BEDIGUNGEN_JAVA_DONE

--- src/IFBedingung.java
+++ src/IFBedingung.java
@@ -1,5 +1,29 @@
+
+
+
+
+
+import java.util.Random;
+
 public class IFBedingung {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Random random = new Random();
+        int randomnumber = random.nextInt(100);
+
+        if(randomnumber<20) {
+            System.out.println("mini");
+
+        } else if ((randomnumber>20) && (randomnumber<50)){
+            System.out.println("Medium");
+
+        } else if (randomnumber<50){
+            System.out.println("Large");
+
+        }
+
+
     }
+
 }
+
+
