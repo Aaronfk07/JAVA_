bb4bf41cf96676bb2fb36282f937757c0bc47a7e
Aaronfk07 <ghost.aaron@gmx.at>
2024-09-20 10:34:40 +0200

    Forloop_JAVA_Aufgabe_1_DONE

--- src/Forloop.java
+++ src/Forloop.java
@@ -1,9 +1,14 @@
+// Zähle in einem For-Loop die Zahlen von 1 bis 100 (inklusive) zusammen
 public class Forloop {
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello" + i);
-            
+
+            for (int j = 0; j < 101; j++) {
+                System.out.println(j);
+
+            }
         }
     }
 }
