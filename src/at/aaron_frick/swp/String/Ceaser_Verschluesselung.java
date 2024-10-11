d3406f087753ef82cc276786c06653ef54a40417
Aaronfk07 <ghost.aaron@gmx.at>
2024-10-11 14:49:47 +0200

    Ceaser_Verschluesselung_encrypted_finished

---
+++ src/at/aaron_frick/swp/String/Ceaser_Verschluesselung.java
@@ -0,0 +1,84 @@
+package at.aaron_frick.swp.String;
+
+import java.util.Scanner;
+
+public class Ceaser_Verschluesselung {
+
+    private static final int SHIFT = 2;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        boolean running = true;
+
+
+        while (running) {
+            System.out.println("Drücken Sie 1 um den Code zu Entrcypten");
+            System.out.println("Drücken Sie 2 um den Code zu Decrypten");
+            System.out.println("Drücken Sie 3 um das Programm zu beenden");
+
+            int selection = scanner.nextInt();
+            scanner.nextLine();
+
+            if (selection == 1) {
+
+                System.out.println("Geben sie den Code den sie encrypten wollen ein");
+                String inputToEncrypt = scanner.nextLine();
+                String encryptedText = encrypt(inputToEncrypt);
+
+
+                System.out.println("Verschlüsselter Text: " + encryptedText);
+            } else if (selection == 2) {
+                System.out.println("Geben Sie den Text ein, den Sie entschlüsseln wollen");
+                //String inputToDecrypt = scanner.nextLine();
+                //String decryptedText = decrypt(inputToDecrypt);
+                //System.out.println("Entschlüsselter Text: " + decryptedText);
+            } else if (selection == 3) {
+                System.out.println("Programm wird beendet");
+                running = false;
+            }
+        }
+        scanner.close();
+
+    }
+    public static String encrypt(String input) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                char shifted = (char) (ch + SHIFT);
+
+
+                if (Character.isLowerCase(ch)) {
+                    if (shifted > 'z') {
+                        shifted = (char) (shifted - 26);
+                    }
+
+                    result.append(shifted);
+                } else if (Character.isUpperCase(ch)) {
+                    if (shifted > 'Z') {
+                        shifted = (char) (shifted - 26);
+                    }
+
+                    result.append(shifted);
+                } else {
+                    result.append(ch);
+                }
+            }
+
+
+        }
+        return result.toString();
+
+        //public static void decrypt (String input){
+           // StringBuilder result1 = new StringBuilder();
+        }
+
+
+    }
+
+
