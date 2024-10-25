e83477e97c0cb17e8338b450ca814efabf8a9641
Aaronfk07 <ghost.aaron@gmx.at>
2024-10-25 14:19:33 +0200

    Erstes_Object_drive

--- src/at/aaron_frick/swp/OO_erstesObjekt/Car.java
+++ src/at/aaron_frick/swp/OO_erstesObjekt/Car.java
@@ -4,5 +4,11 @@ public class Car {
     public int fuelConsumption;
     public String brand;
     public String serialNumber;
+    public int fuelAmount;
     private String color;
+
+    public void drive(){
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("I am Driving");
+    }
 }
--- src/at/aaron_frick/swp/OO_erstesObjekt/Main.java
+++ src/at/aaron_frick/swp/OO_erstesObjekt/Main.java
@@ -9,12 +9,17 @@ public class Main {
         c1.fuelConsumption = 7;
         c1.brand = "Audi";
         c1.serialNumber = "A1234B";
+        c1.fuelAmount = 70;
 
         Car c2 = new Car();
         c2.fuelConsumption = 9;
         c2.brand = "BMW";
         c2.serialNumber = "B2345B";
+        c2.fuelAmount = 45;
 
-        System.out.println(c1.brand);
+
+        System.out.println(c2.fuelAmount);
+        c2.drive();
+        System.out.println(c2.fuelAmount);
     }
 }
