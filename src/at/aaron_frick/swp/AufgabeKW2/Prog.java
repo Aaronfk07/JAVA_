5a0fd0b8502b179ed9c537f3c716be9c8562e0c7
Aaronfk07 <ghost.aaron@gmx.at>
2025-01-10 12:17:37 +0100

    KW2_added_class_Prog

---
+++ src/at/aaron_frick/swp/AufgabeKW2/Prog.java
@@ -0,0 +1,8 @@
+package at.aaron_frick.swp.AufgabeKW2;
+
+class Prog {
+    public static void main(String[] args) {
+        Prog tst1 = new Prog(), tst2 = new Prog();
+        System.out.println(tst1 + "\n"+ tst2);
+    }
+}
