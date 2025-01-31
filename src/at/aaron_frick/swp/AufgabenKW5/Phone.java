1781924ecdc214f7c6a2fd42ae1f855a8234f955
Aaronfk07 <ghost.aaron@gmx.at>
2025-01-31 13:49:39 +0100

    KW5_Aufgabe_2_OO_Interface_finished

---
+++ src/at/aaron_frick/swp/AufgabenKW5/Phone.java
@@ -0,0 +1,28 @@
+package at.aaron_frick.swp.AufgabenKW5;
+
+public class Phone implements Product, HasLogo {
+    private String name;
+    private double price;
+    private String Logo;
+
+    public Phone(String name, double price, String Logo) {
+        this.name = name;
+        this.price = price;
+        this.Logo =Logo;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getLogo() {
+        return Logo;
+    }
+}
