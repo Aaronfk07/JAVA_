1873126e3849ee01124992fa03121690c17273a0
Aaronfk07 <ghost.aaron@gmx.at>
2025-02-07 14:48:01 +0100

    first_Game_moving_rectangle_added_movement

--- Games/src/at/aaron_frick/swp/Rectangle.java
+++ Games/src/at/aaron_frick/swp/Rectangle.java
@@ -5,9 +5,13 @@ import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangle extends BasicGame {
 
+    private float a;
+    private float b;
+
     private float x;
     private float y;
     private float speed;
+    private float backspeed;
 
     public Rectangle(String title) {
         super(title);
@@ -15,27 +19,46 @@ public class Rectangle extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
+   this.a = 50;
+   this.b = 50;
+
     this.x = 100;
     this.y = 100;
-    this.speed = 25.0f;
+    this.speed = 10.0f;
+    this.backspeed = -10f;
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        this.x += (float) delta /this.speed;
-        this.y += (float) delta /this.speed;
-//    this.x += 0.1F;
-//    this.y += 0.1F;
 
-        if(this.x>700 && this.y>500){
-        this.x = 0;
-        this.y = 0;
-     }
+        if(this.x<600) {
+            this.x += (float) delta / this.speed;
+        }
+        if (this.x > 700) {
+            this.x = 600;
+            this.y = 100;
+        }
+        if (this.x > 600) {
+            this.y += (float) delta / this.speed;
+            this.x += 0;
+        }
+
+        if(this.y >600){
+          this.x = 600;
+          this.y = 500;
+        }
+        if(this.y>500){
+            this.x += (float) delta / this.backspeed;
+            this.y= 0;
+        }
+
+//        this.y += 0;
     }
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
         graphics.drawRect(this.x,this.y,100,100);
         graphics.drawString("Dies ist ein Langweiliges Spiel",80,80);
+        graphics.drawRoundRect(this.a,this.b,20,20,25);
     }
 
     public static void main(String[] argv) {
