b310ab3333b39d00aea5ff8ce9df61f6c412beb4
Aaronfk07 <ghost.aaron@gmx.at>
2025-05-23 12:26:39 +0200

    Refactored

--- Java_Games/src/at/aaron_frick/swp/Aufgabe1_Lists/Actor.java
+++
@@ -1,8 +0,0 @@
-package at.aaron_frick.swp.Aufgabe1_Lists;
-
-import org.newdawn.slick.Graphics;
-
-public interface Actor {
-    void render(Graphics graphics);
-    void update(int delta);
-}
--- Java_Games/src/at/aaron_frick/swp/Lists/Actor.java
+++
@@ -1,9 +0,0 @@
-package at.aaron_frick.swp.Lists;
-
-import org.newdawn.slick.Graphics;
-
-public interface Actor {
-
-    public void render(Graphics graphics);
-    public void update(int delta);
-}
